343394c8d6c8f5665f6f45add38b0699a07a5aa3
Mary Béds <MaryBeds@marys-macbook-pro-2.local>
2016-12-06 11:48:45 -0200

    Initial Commit

---
+++ devslopes-social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by Mary Béds on 06/12/16.
+//  Copyright © 2016 Maria Eugênia Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
